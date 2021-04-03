2d9ad0adbff36cc0d36a933b5fc879738cc904da
Raul <rauldavila323@gmail.com>
2021-04-03 21:56:55 +0200

    Routes have been created and backend is completed.

---
+++ backend/controller/productController.js
@@ -0,0 +1,30 @@
+// All fetching is done here
+const Product = require('../models/Product');
+
+const getAllProducts = async(req,res )=>{
+	try{
+		const products = await Product.find({});
+
+		res.json(products)
+	}catch (error){
+		console.log(error);
+		res.status(500).json({message: "Server Error"});
+	}
+}
+
+
+const getProductByID = async (req, res) => {
+	try {
+		const product = await Product.findByID(req.params.id);
+
+		res.json(product)
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Server Error" });
+	}
+}
+
+module.exports = {
+	getAllProducts,
+	getProductByID
+}
