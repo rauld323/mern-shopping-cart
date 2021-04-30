444694dad172a8908aa92a0b59ec75303747183c
Raul <rauldavila323@gmail.com>
2021-04-30 12:08:09 +0200

    Product page has been created

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import './HomeScreen.css'
 
-function HomeScreen() {
+import Product from "../components/Product";
+
+const HomeScreen = () => {
 	return (
-		<div className="homeScreen">
-			Home Page
+		<div className="homescreen">
+			<h2 className="homescreen_title">Lates Prodcuts</h2>
+			<div className="homescreen_products">
+				<Product />
+			</div>
 		</div>
 	)
+
 }
 
 export default HomeScreen
