3503021f304e5caa3fbb21d890233b98245d7197
Raul <rauldavila323@gmail.com>
2021-06-08 10:54:00 +0200

    Product page has been styled and made responsive.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,23 +1,37 @@
 import './Product.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Product = () => {
 	return (
-		<div className="product">
-			<img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="product name"/>
+		<div className='product'>
+			<img
+				src='https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+				alt='product name'
+			/>
 
-				<div className="product_info">
-					<p className="info_name">Product 1</p>
-					<p className="info_description">
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa nemo officiis saepe excepturi rerum possimus quidem pariatur quas deleniti earum, culpa qui ea voluptates perspiciatis error dicta ducimus amet adipisci.
-					</p>
+			<div className='product_info'>
+				<p className='info_name'>Product 1</p>
+				<p className='info_description'>
+					Lorem ipsum dolor sit, amet
+					consectetur adipisicing elit.
+					Ipsa nemo officiis saepe
+					excepturi rerum possimus
+					quidem pariatur quas deleniti
+					earum, culpa qui ea voluptates
+					perspiciatis error dicta
+					ducimus amet adipisci.
+				</p>
 
-					<p className="info_price">$20.00</p>
+				<p className='info_price'>$20.00</p>
 
-					<Link to={`/product/${2222}`} className="info_button">View</Link>
-				</div>
+				<Link
+					to={`/product/${2222}`}
+					className='info_button'>
+					View
+				</Link>
+			</div>
 		</div>
-	)
-}
-
-export default Product
+	);
+};
+// 1:40
+export default Product;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,25 @@
-import './HomeScreen.css'
+import './HomeScreen.css';
 
-import Product from "../components/Product";
+import Product from '../components/Product';
 
 const HomeScreen = () => {
 	return (
-		<div className="homescreen">
-			<h2 className="homescreen_title">Lates Prodcuts</h2>
-			<div className="homescreen_products">
+		<div className='homescreen'>
+			<h2 className='homescreen_title'>
+				Latest Products
+			</h2>
+			<div className='homescreen_products'>
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
+				<Product />
 				<Product />
 			</div>
 		</div>
-	)
+	);
+};
 
-}
-
-export default HomeScreen
+export default HomeScreen;
