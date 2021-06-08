573a8f181edce1bb7a59129638bf3f1391f56922
Raul <rauldavila323@gmail.com>
2021-06-08 12:31:54 +0200

    CartScreen has been created/styled. CartItem has been created

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,26 @@
-import './CartScreen.css'
+import './CartScreen.css';
+import CartItem from '../components/CartItem';
 
 function CartScreen() {
 	return (
-		<div className="cartScreen">
-			Cart Window
+		<div className='cartScreen'>
+			<div className='cartscreen_left'>
+				<h2>Shopping Cart</h2>
+				<CartItem />
+			</div>
+			<div className='cartscreen_right'>
+				<div className='cartscreen_info'>
+					<p>Subtotal (0) items</p>
+					<p>$56.99</p>
+				</div>
+				<div>
+					<button>
+						Proceed To Checkout
+					</button>
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
 
-export default CartScreen
+export default CartScreen;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,67 @@
-import './ProductScreen.css'
+import './ProductScreen.css';
 
 function ProductScreen() {
 	return (
-		<div className="productScreen">
-			Product Window
+		<div className='productscreen'>
+			<div className='productscreen_left'>
+				<div className='left_image'>
+					<img
+						src='https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+						alt='product Name'
+					/>
+				</div>
+				<div className='left_info'>
+					<p className='left_name'>
+						PlayStation
+					</p>
+					<p>Price: 400</p>
+					<p>
+						Lorem ipsum dolor
+						sit amet consectetur
+						adipisicing elit.
+						Quis eaque natus{' '}
+					</p>
+				</div>
+			</div>
+			<div className='productscreen_right'>
+				<div className='right_info'>
+					<p>
+						Price:
+						<span>500</span>
+					</p>
+					<p>
+						Status:
+						<span>
+							In stock
+						</span>
+					</p>
+					<p>
+						Qty:
+						<select>
+							<option value='1'>
+								1
+							</option>
+							<option value='2'>
+								2
+							</option>
+							<option value='3'>
+								3
+							</option>
+							<option value='4'>
+								4
+							</option>
+						</select>
+					</p>
+					<p>
+						<button type='button'>
+							Add To
+							Cart
+						</button>
+					</p>
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
 
-export default ProductScreen
+export default ProductScreen;
